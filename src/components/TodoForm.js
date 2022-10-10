a2df89c05155f4d697e2b0fe74ff73f7e00361b7
Paul  Van Dyke <paulvandyke@Pauls-Air.attlocal.net>
2022-10-10 11:24:52 -0400

    Basic todo App working

---
+++ src/components/TodoForm.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect, useRef } from 'react'
+
+const TodoForm = props => {
+const [input, setInput] = useState(props.edit ? props.edit.value : ''); //Ternary takes the current value of the todo and passes it as a placeholder on edit
+
+const inputRef = useRef(null); 
+
+useEffect(() => inputRef.current.focus());
+
+const handleChange = e => setInput(e.target.value);
+
+
+const handleSubmit = e => {
+    e.preventDefault(); 
+    props.onSubmit({
+        id: Math.floor(Math.random() * 10000),
+        text: input
+    });
+    setInput(''); 
+}
+
+  return (
+    <form className='todo-form' onSubmit={handleSubmit} >  
+        {props.edit ? (<><input
+              type="text"
+              placeholder="Update a todo"
+              value={input}
+              name='text'
+              className='todo-input edit'
+              onChange={handleChange}
+              ref={inputRef} /><button className='todo-button edit'>Edit todo </button></> ) :
+        (   <><input
+                  type="text"
+                  placeholder="Add a todo"
+                  value={input}
+                  name='text'
+                  className='todo-input'
+                  onChange={handleChange}
+                  ref={inputRef} /><button className='todo-button'>Add todo </button></>
+        )}   
+        
+    </form>
+  )
+}
+
+export default TodoForm
